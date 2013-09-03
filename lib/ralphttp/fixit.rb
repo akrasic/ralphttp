611c1550867c7f1de49c875a442369e6e174cc82
Antun Krasic <antun@martuna.co>
2013-09-03 11:26:44 +0200

    Documentation change

--- lib/ralphttp/fixit.rb
+++ lib/ralphttp/fixit.rb
@@ -1,10 +1,14 @@
 module Ralphttp
-  # Public - Format data for a reasonable output
-
+  # Public: Format data for a reasonable output
   class Fixit
     attr_accessor :json
     attr_accessor :csv
 
+    # Public: Start the class based on selected options
+    #
+    # options - Hash containing settings
+    #
+    # Returns nil
     def initialize(options)
       @json = options[:json] unless options[:json].nil?
       @csv = options[:csv] unless options[:csv].nil?
