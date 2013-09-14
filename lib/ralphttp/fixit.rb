bdc13de24295615ab5d2e701591198e17259a1d1
Antun Krasic <antun@martuna.co>
2013-09-14 03:51:00 +0200

    Refactor the code

--- lib/ralphttp/fixit.rb
+++ lib/ralphttp/fixit.rb
@@ -3,9 +3,12 @@ module Ralphttp
   class Fixit
     attr_accessor :detailed
     attr_accessor :csv
+    attr_accessor :csvexport
     attr_accessor :bucket
     attr_accessor :total_time
     attr_accessor :status
+    attr_accessor :error
+    attr_accessor :req
 
     # Public: Start the class based on selected options
     #
@@ -14,56 +17,101 @@ module Ralphttp
     # Returns nil
     def initialize(options)
       @status = {}
+      @error = []
       @detailed = options[:detail]
-      @csv = options[:csv] unless options[:csv].nil?
+
+      unless options[:csv].nil?
+        @csv = options[:csv]
+        assign_csv_report
+      end
     end
 
+    # Public: Analyze collected data and show reasonable output
+    #
+    # Return Text output
+    def analyze
+      if @error.empty?
+        display_results
+      else
+        puts 'Errors encountered when connecting:'
+        @error.each do |e|
+          puts e
+        end
+      end
+    end
+
+    # Public - Displays the results of the parsed data
+    #
+    # Returns nil
+    def display_results
+      print_header
+      print_detailed_report
+      write_csv_report
+      display_status
+    end
 
     # Public: Output the header information
     #
     # Returns nil
     def print_header
       unless @detailed.nil?
-        puts '%-30s %-10s %-10s' % ['Time', 'Req/s', 'Avg resp (ms)']
+        puts sprintf('%-30s %-10s %-10s', 'Time', 'Req/s', 'Avg resp (ms)')
       end
     end
-    # Public: Analyze collected data and show reasonable output
+
+    private
+
+    # Private - Print the status banner
     #
-    # Return Text output
-    def analyze
-      @bucket.keys.sort
+    # Returns nil
+    def display_status
+      reqs_per_sec = sprintf('%.2f',
+                             (@req.inject(:+).to_f / @req.length.to_f))
 
-      req = []
-      ms = []
+      puts "\nRequests per second: #{reqs_per_sec}"
+      @status.map do |status_code, status_count|
+        puts "HTTP Status #{status_code}: #{status_count}"
+      end
+      puts "Total time: #{@total_time} s"
+    end
 
+    #
+    # Private - Assign Ralphttp::CsvExport class if report is asked for
+    #
+    # Returns nil
+    def assign_csv_report
       unless @csv.nil?
         csv_header = ['Time', 'Req/s', 'Avg. resp. (ms)']
-        csv = Ralphttp::CsvExport.new(csv_header)
+        @csvexport = Ralphttp::CsvExport.new(csv_header)
       end
+    end
 
-      print_header
-      @bucket.map do |x, y|
-        reqs = y.length
-        req << reqs
-        date = Time.at(x)
-        ms = calc_response(y)
+    # Private - Write down the CSV report
+    #
+    # Returns nil
+    def write_csv_report
+      unless @csv.nil?
+        @csvexport.write(@csv)
+      end
+    end
 
-        csv.add_row([date, reqs, ms]) unless @csv.nil?
+    # Private - Prints out a detailed report, if asked for
+    #
+    # Returns Array containing summarized data
+    def print_detailed_report
+      @req = []
+      @bucket.map do |time, data|
+        reqs = data.length
+        @req << reqs
+        date = Time.at(time)
+        ms = calc_response(data)
+
+        @csvexport.add_row([date, reqs, ms]) unless @csv.nil?
 
         puts sprintf('%-30s %-10s %-20s',
                      date, reqs, ms) unless @detailed.nil?
       end
-      reqs_per_sec = sprintf('%.2f', (req.inject(:+).to_f / req.length.to_f))
-
-      puts "\nRequests per second: #{reqs_per_sec}"
-      @status.map { |x, y| puts "HTTP Status #{x}: #{y}" }
-      puts "Total time: #{@total_time} s"
-
-      csv.write(@csv)
-
     end
-
-    private
 
     # Private: Calculate average response time in ms
     #
--- lib/ralphttp/wreckit.rb
+++ lib/ralphttp/wreckit.rb
@@ -34,38 +34,25 @@ module Ralphttp
   #
   # Returns nil
   def blast
-    begin
-      arr = []
-      start = Time.now
-      loopy = calculate_loops
+    arr = []
+    start = Time.now
+    num_loops = calculate_loops
 
-      (1..loopy.to_i).each do |f|
-        Thread.abort_on_exception = false
-        (1..@hammer[:concurrent].to_i).each do |s|
-          arr[s] = Thread.new do
-            k = getpage
-            Thread.current['response'] = k['response']
-            Thread.current['time'] = k['time']
-            Thread.current['date'] = k['date']
-          end
+    (1..num_loops.to_i).each do |f|
+      Thread.abort_on_exception = false
+      (1..@hammer[:concurrent].to_i).each do |s|
+        arr[s] = Thread.new do
+          start_http
         end
-
-        arr.shift
-        # Join the hands and go ahead
-        arr.each do |t|
-          t.join
-          # puts "#{t['date']} :: #{t['response']} :: #{t['time']} ms"
-        end
-
-      # End initial loop
       end
-      @total_time = sprintf('%.2f', (Time.now - start))
-    rescue ThreadError => e
-      puts "Ralphttp::Wreckit error: #{e}"
-    rescue SocketError => e
-      puts "Can not connect to the URL"
-      exit
+
+      arr.shift
+      arr.each do |t|
+        t.join
+      end
     end
+    @total_time = sprintf('%.2f', (Time.now - start))
+    @bucket.keys.sort
   end
 
     private
@@ -73,56 +60,58 @@ module Ralphttp
   # Private: Calculate number of loops based on total number of connections
   # opposed to the total number of concurrent connections
   #
-  #
-
   def calculate_loops
     @hammer[:total].to_i / @hammer[:concurrent].to_i
-  end
-
-  # Private - Returns metrics for conntacting a page
-  #
-  # Returns Hash with response code and time (in ms)
-  def getpage
-    begin
-      out = start_http
-    rescue Errno::ECONNREFUSED
-      puts 'Connection refused'
-    rescue Errno::ETIMEDOUT
-      puts 'Can not connect to the provided URL'
-    end
-    out
   end
 
   # Private: Opens the domain for testing and gathers the performance info
   #
   # Returns Hash with response code and time in ms
   def start_http
-    out = {}
+    begin
+      Net::HTTP.start(@domain.host, @domain.port) do |http|
+        start_request = Time.now
+        request = Net::HTTP::Get.new(@domain.request_uri)
+        request.add_field('User-Agent', return_user_agent)
+        end_request = sprintf('%.4f', ((Time.now - start_request) * (10**6)))
+
+        response = http.request request
+        http_status_counter(response.code)
+        apply_processed_data([response.code, end_request])
+      end
+    rescue Errno::ECONNREFUSED
+      @error << 'Connection refused'
+    rescue Errno::ETIMEDOUT
+      @error << 'Can not connect to the provided URL'
+    rescue Net::ReadTimeout
+      @error << 'ReadTimeout'
+    rescue SocketError
+      @error << 'Non resolvable domain!'
+    end
+  end
+
+  # Private - Set the User Agent string for the Net::HTTP module
+  #
+  # Returns String User Agent
+  def return_user_agent
     if @ua.nil?
       user_agent = { 'User-Agent' => 'Ralphttp-Wreck' }
     else
       user_agent = { 'User-Agent' => @ua }
     end
+  end
 
-    Net::HTTP.start(@domain.host, @domain.port) do |http|
-      start_request = Time.now
-      request = Net::HTTP::Get.new(@domain.request_uri)
-      request.add_field('User-Agent', user_agent)
-      end_request = sprintf('%.4f', ((Time.now - start_request) * (10**6)))
-
-      response = http.request request
-      http_status_counter(response.code)
-
-      out['response'] = response.code
-      out['time'] = end_request
-      out['date'] = Time.now.to_i
-
+  # Private - Fill @bucket with statistical data, the response code received
+  # and the time that took to complete the HTTP request.
+  #
+  # droplet - Array holding the HTTP reponse code and HTTP request duration
+  #
+  # Returns nil
+  def apply_processed_data(droplet)
       unless @bucket[Time.now.to_i].kind_of?(Array)
         @bucket[Time.now.to_i] = []
       end
-      @bucket[Time.now.to_i] << [response.code, end_request]
-    end
-    out
+      @bucket[Time.now.to_i] << droplet
   end
 
   # Private: Get number of HTTP status codes and their count
